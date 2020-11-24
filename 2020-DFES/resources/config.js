724783a9079e39ef2e86835cb761fa3a733e221a
slowe <gravatar@strudel.org.uk>
2020-11-24 17:05:46 +0000

    Add County mapping for #10

--- 2020-DFES/resources/config.js
+++ 2020-DFES/resources/config.js
@@ -101,10 +101,10 @@ S(document).ready(function(){
 			},
 			"Countylayer":{
 				"geojson": "data/maps/Counties-clipped-simplified.geojson",
-				"key": "cty20cd",
+				"key": "cty19cd",
 				"data": {
 					"mapping": {
-						"src": "data/lsoa2lep-compact.json",
+						"src": "data/lsoa2cty.json",
 						"process": function(d){
 							// Work out mapping from LSOA to LAD
 							// Data is saved as { LAD: [LSOA1,LSOA2,LSOA3...] }
@@ -202,8 +202,8 @@ S(document).ready(function(){
 				}],
 				"popup": {
 					"text": function(attr){
-						file = 'County-'+attr.properties.cty20cd+'-'+this.options.scenario.replace(/ /,"").toLowerCase()+'-'+this.options.parameter+'.png';
-						return '<h3>'+(attr.properties.cty20nm || '?')+'</h3><p>'+attr.parameter.title+': '+attr.value.toLocaleString()+attr.parameter.units+' ('+this.options.key+')</p><div id="barchart">barchart</div><p class="footnote">The area has been clipped to UKPN\'s area</p><p class="footnote capture-hide"><a href="#" onClick="saveDOMImage(document.querySelector(\'.dfes-popup-content\'),{\'src\':\''+file+'\',\'scale\':true});">Save chart as PNG</a></p>';
+						file = 'County-'+attr.properties.cty19cd+'-'+this.options.scenario.replace(/ /,"").toLowerCase()+'-'+this.options.parameter+'.png';
+						return '<h3>'+(attr.properties.cty19nm || '?')+'</h3><p>'+attr.parameter.title+': '+attr.value.toLocaleString()+attr.parameter.units+' ('+this.options.key+')</p><div id="barchart">barchart</div><p class="footnote">The area has been clipped to UKPN\'s area</p><p class="footnote capture-hide"><a href="#" onClick="saveDOMImage(document.querySelector(\'.dfes-popup-content\'),{\'src\':\''+file+'\',\'scale\':true});">Save chart as PNG</a></p>';
 					},
 					"open": function(attr){
 						var data,c,p,key,values,l;
